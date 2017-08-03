c57fdf8f89f74295034c291c608a2033acb4a1b6
Pavel Davydov <pdavydov108@gmail.com>
2017-08-03 20:06:57 +0300

    add benchmark that reads 1MB file

--- file.cpp
+++ file.cpp
@@ -1,7 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include <fstream>
 #include <cstdio>
+#include <fstream>
 
 void BM_file_open_close(benchmark::State &state) {
     std::ios::sync_with_stdio(true);
@@ -18,6 +18,21 @@ void BM_file_open_close_no_sync(benchmark::State &state) {
     }
 }
 BENCHMARK(BM_file_open_close_no_sync);
+
+void BM_file_read_1M(benchmark::State &state) {
+    std::vector<char> buffer(1024 * 1024, 0);
+    {
+        std::fstream f("text");
+        f.write(buffer.data(), buffer.size());
+    }
+    std::ios::sync_with_stdio(false);
+    while (state.KeepRunning()) {
+        std::fstream f("text");
+        f.read(buffer.data(), buffer.size());
+    }
+    remove("text");
+}
+BENCHMARK(BM_file_read_1M);
 
 void BM_cfile_open_close(benchmark::State &state) {
     while (state.KeepRunning()) {
